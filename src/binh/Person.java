f299bac3c4aa57e02d9820d1751d6e16a9bad3e0
daovanhuy209 <daovanhuy2513@gmail.com>
2024-12-17 10:13:22 +0700

    Add files via upload

---
+++ src/binh/Person.java
@@ -0,0 +1,24 @@
+package binh;
+
+import java.util.Iterator;
+
+public class Person {
+	/**
+	 * Bien instance / bien doi tuong 
+	 */
+	public String name;
+	public int old;
+	
+	public static String nameSchool = "HUE UIN";
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+		//old la bien cuc bo nam trong ham khoi tao
+		this.name = "";
+		this.old =10;
+	}
+	
+	public void show() {
+		System.out.println();
+	}
+}
